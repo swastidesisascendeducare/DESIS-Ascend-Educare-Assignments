92561a0ebdb410d32010704e8779c6cf68750161
Swasti Mishra <swasti.mishra@desisascendeducare.in>
2025-01-19 01:10:58 +0530

    deleted unnecessary folder

--- Assignment 3 DBMS/Queries For airportdb/AvgPriceGivenFlightID.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-    flight_id,
-    AVG(price) AS average_booking_price
-FROM 
-    booking
-WHERE 
-    flight_id = 3863
-GROUP BY 
-    flight_id;
--- Assignment 3 DBMS/Queries For airportdb/DatesNotSuitableForFlights.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-    log_date, COUNT(*) AS unsuitable_days
-FROM 
-    weatherdata
-WHERE 
-    airpressure > 1015 AND 
-    humidity >= 98
-GROUP BY 
-    log_date;
